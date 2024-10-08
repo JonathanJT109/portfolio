a3eab37a23f6968b59a8b606d4201fbe37ee9ae2
Jonathan Gonzalez <jonathangon.2014@gmail.com>
2024-10-07 21:39:16 -0400

    Added success when fail submission and console.log

--- app/api/form/route.js
+++ app/api/form/route.js
@@ -10,10 +10,11 @@ export async function POST(request) {
         });
 
         const data = await response.json();
+        console.log(data.message);
         if (data.success) {
                 return NextResponse.json({ message: "Form submitted successfully", success: true }, { status: 200 });
         }
         else {
-                return NextResponse.json({ message: "Failed to submit form" });
+                return NextResponse.json({ message: "Failed to submit form", success: false });
         }
 }
